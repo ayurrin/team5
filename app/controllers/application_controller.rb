9da7865903df9a6a3aec9e8e65edc4dec1212193
sevan64 <42733539+sevan64@users.noreply.github.com>
2019-12-20 16:01:37 +0900

    パスワードリセットの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name]) # 新規登録時(sign_up時)にnameというキーのパラメーターを追加で許可する
   end
-  
+  def set_host
+    Rails.application.routes.default_url_options[:host] = request.host_with_port
+  end
 end
