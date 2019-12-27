c33a72b0297d6121a8d050348466a9f9c8388d54
sevan64 <42733539+sevan64@users.noreply.github.com>
2019-12-27 15:01:34 +0900

    ログイン前に新規投稿できる問題を修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,18 @@
 class PostsController < ApplicationController
+
+  before_action :sakilog, except: :index
+
   def index
     @posts = Post.all.order(created_at: :desc)
   end
   def show
     @post = Post.find_by(id: params[:id])
   end
-  
+
   def new
     @post = Post.new
   end
-  
+
   def create
     @post = Post.new(post_params)
     @post.save
@@ -19,19 +22,19 @@ class PostsController < ApplicationController
   def edit
     @post = Post.find_by(id: params[:id])
   end
-  
+
   def update
     @post = Post.find_by(id: params[:id])
     @post.content = params[:content]
     @post.save
     redirect_to("/posts/index")
   end
-  
+
   def destroy
     # destroyアクションの中身を作成してください
     @post=Post.find_by(id: params[:id])
     @post.destroy
-    
+
     redirect_to("/posts/index")
   end
 
@@ -39,8 +42,11 @@ private
 
     def post_params
       params.require(:post).permit(:content, :image)
-    end  
+    end
+
+    def sakilog
+      redirect_to action: :index unless user_signed_in?
+    end
 
 
-  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'home/top' => "home#top"
   resource :passwords, only: [:create]
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/' => "home#top"
 
