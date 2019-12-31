49bf9709b926ea954baab11ef2aecea8439f51b3
sevan64 <42733539+sevan64@users.noreply.github.com>
2020-01-01 02:57:32 +0900

    フォロー機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,22 @@
 Rails.application.routes.draw do
   get "posts/index" => "posts#index"
 
-  devise_for :users
+  devise_for :users, :controllers => {
+    :sessions      => "users/sessions",
+    :registrations => "users/registrations",
+    :passwords     => "users/passwords",
+  }
+  resources :users, only: [:index, :show]
+  put 'users/follow/:user_id' => 'users#follow'
+  put 'users/unfollow/:user_id' => 'users#unfollow'
+  #フォローとフォローを外すアクション
+
+  get 'users/follow_list/:user_id' => 'users#follow_list'
+  get 'users/follower_list/:user_id' => 'users#follower_list'
+  #フォロー・フォロワーの一覧ページ
   get 'home/top' => "home#top"
   resource :passwords, only: [:create]
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/' => "home#top"
 
