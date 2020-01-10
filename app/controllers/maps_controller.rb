1bdc8550968f1915cc013784e430f2a39c7168b4
ayurin <sports128dream@gmail.com>
2020-01-10 12:40:59 +0900

    address変更

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,7 +1,7 @@
 class MapsController < ApplicationController
   def index        
     @user = User.find(params[:id])
-    gon.posts_contents=@user.liked_posts.pluck(:content)
+    gon.posts_contents=@user.liked_posts.pluck(:address)
   end
 
 
